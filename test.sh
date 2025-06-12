8ec20e73ff5188c857216b0e9147f32e14f791fa
Patrick Butler <pbutler@killertux.org>
2025-06-12 12:37:28 -0400

    fix: bash test.hs

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ uv pip install ruff
 
 ruff check benchmark.py
 
-DISK="df -k / | cut -w -f4 | tail -n1"
+DISK=$(df -k / | cut -w -f4 | tail -n1)
 if [[ ${DISK} -lt 20971520 ]]; then
   echo "Disk space is less than 20GB, skipping benchmark."
   exit 1
