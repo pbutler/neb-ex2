a112f0b51996190461b7748de9ed660ba1f2a123
Patrick Butler <pbutler@killertux.org>
2025-06-12 12:27:51 -0400

    fix: test case
    it's silly but it's testing something

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+uv pip install ruff
+
+ruff check benchmark.py
+
+DISK="df -k / | cut -w -f4 | tail -n1"
+if [[ ${DISK} -lt 20971520 ]]; then
+  echo "Disk space is less than 20GB, skipping benchmark."
+  exit 1
+fi
